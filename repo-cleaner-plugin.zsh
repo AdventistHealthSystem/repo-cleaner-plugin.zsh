fe67d1e90ff0a26b84f2e506770ca25e541d1ca5
Cory Collier <cory.collier@ahss.org>
2016-09-30 10:54:30 -0400

    First commit of the plugin

---
+++ repo-cleaner-plugin.zsh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+
+# Iterates through given folders
+repo-cleaner-iterate() {
+    RESET="\033[0m"
+    for DIR in "$@";
+    do
+        cd "$DIR/../"
+        BRANCH="$(git name-rev --name-only HEAD)"
+        echo -e "${RESET}====================================================================="
+        echo "$DIR [$BRANCH]"
+        repo-cleaner-update
+    done
+}
+
+# Updates all of the git stuff in a folder
+repo-cleaner-update() {
+    RED="\033[01;31m"
+
+    git fetch origin
+    git pull origin $BRANCH
+    echo -e "\n${RED}git remote prune origin"
+    git prune
+    git gc --aggressive
+    git fsck --full
+}
+
+repo-cleaner-iterate $(find $HOME -name .git -type d)
